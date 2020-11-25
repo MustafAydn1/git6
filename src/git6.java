32294a05d37c3e06bb575adfc2e798f9ad4dccee
Mustafa Aydin <mustafakilic3520@gmail.com>
2020-11-25 21:20:05 +0300

    git yol haritamız

--- src/git6.java
+++ src/git6.java
@@ -2,5 +2,24 @@ public class git6 {
     public static void main(String[] args) {
         System.out.println("git4 ignoreler");
         System.out.println("en son ekledim mi");
+
+        System.out.println("1- Kendi projelerimizi geliştirmek ve sunmak için.\n" +
+                "   a) Git komut satırından , komutlarla bu işi yapacağız.\n" +
+                "   b) IntellijIdea yani IDE den yapacağız.\n" +
+                "\n" +
+                "2- Ders sistemine uyugn halini yapacağız. yani hoca yazacak\n" +
+                "   biz kendi kodumuzu günceleyeceğiz.\n" +
+                "\n" +
+                "3- Grup çalışması için gereken ortamı oluşturacağız.");
     }
 }
+/*
+1- Kendi projelerimizi geliştirmek ve sunmak için.
+   a) Git komut satırından , komutlarla bu işi yapacağız.
+   b) IntellijIdea yani IDE den yapacağız.
+
+2- Ders sistemine uyugn halini yapacağız. yani hoca yazacak
+   biz kendi kodumuzu günceleyeceğiz.
+
+3- Grup çalışması için gereken ortamı oluşturacağız.
+ */
